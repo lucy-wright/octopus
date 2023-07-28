f3cc21f1313434430df12f99db876025aee79d28
Lucy Wright <lucy412@icloud.com>
2023-07-28 16:13:09 +0100

    testing embedding matplotlib plot into html

---
+++ plots/example.py
@@ -0,0 +1,28 @@
+#######################
+#Import dependencies
+########################
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import mpld3
+
+x = [1, 2, 3, 4, 5, 6]
+y = [1, 4, 9, 16, 25, 36]
+
+fig, ax1 = plt.subplots()
+ax1.plot(x, y)
+
+
+
+################################################
+#Save the figure to our local machine
+################################################
+
+plt.savefig('xyplot.png')
+
+
+# Transform to HTML code
+html_str = mpld3.fig_to_html(fig)
+Html_file= open("plot.html","w")
+Html_file.write(html_str)
+Html_file.close()
